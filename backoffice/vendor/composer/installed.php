0aed44182e7deee4538278c41cd35f7214fc05b3
Alexandre Osovski <74330383+AlexandreOsovski@users.noreply.github.com>
2023-11-30 02:55:54 -0300

    fix: projeto iniciado

---
+++ backoffice/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alexa/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '69bce001b81d962431db3e2bda91938eec369356',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alexa/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '69bce001b81d962431db3e2bda91938eec369356',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'league/plates' => array(
+            'pretty_version' => 'v3.5.0',
+            'version' => '3.5.0.0',
+            'reference' => 'a6a3238e46c6e19af7318fdc36bfbe49b0620231',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/plates',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
